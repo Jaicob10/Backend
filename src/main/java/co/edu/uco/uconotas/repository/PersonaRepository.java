1e132c33bb54f7541c3822e734973e74c5223570
Santiago Ríos Ríos <36767626+Jaicob10@users.noreply.github.com>
2019-11-07 14:29:46 -0500

    Se crea el repositorio para buscar por Id y Tipo-Id

--- src/main/java/co/edu/uco/uconotas/repository/PersonaRepository.java
+++ src/main/java/co/edu/uco/uconotas/repository/PersonaRepository.java
@@ -1,9 +1,15 @@
 package co.edu.uco.uconotas.repository;
 
 import co.edu.uco.uconotas.entity.PersonaEntity;
+import co.edu.uco.uconotas.entity.TipoDocumentoIdentidadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonaRepository extends JpaRepository<PersonaEntity,Long> {
+
+    List<PersonaEntity> findByTipoDocIdentidadAndNroDocumento
+            (TipoDocumentoIdentidadEntity TipoDocumentoIdentidad, String NroDocumento);
 }
